2e1a9f3114976c3f5b5d7a3398c8c83e065bf90c
Noah Heinrich <noahheinrich@gmail.com>
2017-12-09 17:18:34 -0600

    Add generate method with pseudocode

--- cell.rb
+++ cell.rb
@@ -4,4 +4,17 @@ class Cell
     @alive = status
   end
 
+  def generate(neighbors)
+    #if cell is alive
+    #  if neighbors < 2 OR neighbors > 3
+    #    return dead cell
+    #  else
+    #    return live cell
+    #else
+    #  if neighbors == 3
+    #    return live cell
+    #  else
+    #    return dead cell
+  end
+
 end

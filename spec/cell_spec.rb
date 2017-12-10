5f66b54a7b49004b9605039114d59c0411dae00b
Noah Heinrich <noahheinrich@gmail.com>
2017-12-09 19:08:18 -0600

    added tests for generate with a live cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -21,4 +21,26 @@ describe Cell do
       expect(@dead_cell).to be_false
     end
   end
+
+  describe "#generate" do
+    it "returns a Cell object" do
+      new_cell = @live_cell.generate(2)
+      expect(new_cell).to be_a Cell
+    end
+
+    it "returns a live Cell if living cell has 2 neighbors" do
+      new_cell = @live_cell.generate(2)
+      expect(new_cell.alive).to be_true
+    end
+
+    it "returns a dead Cell if living cell has less than 2 neighbors" do
+      new_cell = @live_cell.generate(1)
+      expect(new_cell.alive).to be_false
+    end
+
+    it "returns a dead Cell if living cell has more than 3 neighbors" do
+      new_cell = @live_cell.generate(4)
+      expect(new_cell.alive).to be_false
+    end
+  end
 end

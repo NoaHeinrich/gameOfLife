1ac588a37f3d2d4fbb3c388b450fd928cce90aab
Noah Heinrich <noahheinrich@gmail.com>
2017-12-10 12:03:22 -0600

    runner works as expected

--- runner.rb
+++ runner.rb
@@ -8,3 +8,14 @@ width = gets.chomp.to_i
 game = Game.new(height, width)
 puts "Thank you. Here is your starting grid."
 puts game
+
+loop_status = "y"
+generations = 0
+while loop_status == "y" do
+  game.play_tick
+  generations += 1
+  puts "Generation: " + generations.to_s
+  puts game
+  puts "Continue? (y/n)"
+  loop_status = gets.chomp
+end

f7e6cde2d7d4466372b00c42d2a8d11745141247
Noah Heinrich <noahheinrich@gmail.com>
2017-12-09 18:02:35 -0600

    Added count neighbors method with pseudocode

--- game.rb
+++ game.rb
@@ -5,4 +5,13 @@ class Game
     @width = width
     @grid = Array.new(height) {Array.new(width) { Cell.new } }
   end
+
+  def count_neighbors
+    #sum = 0
+    #new_grid = []
+    #@grid.each do
+    #  row.each do
+    #    if not out of bounds, get each neighbor
+
+  end
 end
